ddeb4b554f81bb31de3fe8786b1eb19945ab177d
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-11-17 12:13:16 +0100

    Change module paths to fork
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -14,8 +14,8 @@ import (
 
 	"golang.org/x/term"
 
-	"mvdan.cc/sh/v3/interp"
-	"mvdan.cc/sh/v3/syntax"
+	"github.com/katexochen/sh/v3/interp"
+	"github.com/katexochen/sh/v3/syntax"
 )
 
 var command = flag.String("c", "", "command to be executed")
